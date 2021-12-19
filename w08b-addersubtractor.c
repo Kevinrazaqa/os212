fccd9502ff2af4caa36c44fcb8b1b401e1230c51
kevinrazaqa <70851000+Kevinrazaqa@users.noreply.github.com>
2021-12-19 14:19:45 +0700

    Add files via upload

---
+++ w08b-addersubtractor.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "99-myutils.h"
+
+
+#define T_REHAT 2000
+
+int idmaster = 0;
+int sharedInt = 0;
+
+
+void* adder (void* a) {
+
+}
+
+
+void* substractor (void* a) {
+
+}
+
+
+
+int main(int argc, char * argv[])
+{  
+   int n;
+
+
+   printf("Hello... \n");
+   printf("Please input a positive number (N) : ");
+   scanf("%d", &n);
+   printf("The input N is : %d \n", n);
+
+   daftar_trit(adder);
+   daftar_trit(substractor);
+
+   jalankan_trit();
+   beberes_trit("Selesai...");
+}
